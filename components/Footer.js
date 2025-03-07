dd69f63d2f4937f3f4b0edc3d4fbca46b0943068
umjoshwu <joshwu@umich.edu>
2025-03-06 21:58:14 -0500

    d

--- components/Footer.js
+++ components/Footer.js
@@ -31,20 +31,5 @@ export const Contact = ({ title, description, buttons }) => {
 }
 
 export const Footer = () => {
-	return (
-		<footer className="bg-secondary text-center py-2 px-5">
-			<div className="container text-muted">
-				<small>&copy; 2021 {" "}
-					<Link href="https://github.com/hashirshoaeb">
-						<a>hashirshoaeb</a>
-					</Link>
-					. Open sourced with love under {" "}
-					<Link href="https://github.com/hashirshoaeb/portfolio/blob/main/LICENSE">
-						<a>MIT</a>
-					</Link>
-					{" "} License
-				</small>
-			</div>
-		</footer>
-	);
+
 }
